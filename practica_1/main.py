cbcbfb7c3d7a156d4f1c0bcf90b975c97bac8b50
hecmung <kenwey3018@gmail.com>
2023-09-06 09:47:49 -0600

    Codigo sin modificar de la practica 1

---
+++ practica_1/main.py
@@ -0,0 +1,32 @@
+// Ustedes deberán realizar los cambios en caso de ser necesarios
+// para quitar los errores y obtener la ejecución del agente inteligente
+import random
+class GuessingAgent:
+def __init__(self, min_range, max_range):
+self.min_range = min_range
+self.max_range = max_range
+def guess(self):
+return random.randint(self.min_range, self.max_range)
+def main():
+print("Bienvenido al juego 'Adivina el número'!")
+min_range = 1
+max_range = 100
+number_to_guess = random.randint(min_range, max_range)
+agent = GuessingAgent(min_range, max_range)
+
+while True:
+agent_guess = agent.guess()
+print(f"El agente adivina: {agent_guess}")
+user_response = input("Es demasiado alto (A), demasiado bajo (B) o correcto (C)?
+").upper()
+if user_response == "A":
+agent.max_range = agent_guess - 1
+elif user_response == "B":
+agent.min_range = agent_guess + 1
+elif user_response == "C":
+print("El agente ha adivinado el número correctamente!")
+break
+else:
+print("Respuesta no válida. Por favor, responde con A, B o C.")
+if __name__ == "__main__":
+main()
