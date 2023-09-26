e1b522ce2a0d94314f6eb49d9ab4ef9084c1b424
Hector Munguia <kenwey3018@gmail.com>
2023-09-25 20:37:31 -0600

    Codigo modificado v1

--- p2_busqueda_no_informada.py
+++ p2_busqueda_no_informada.py
@@ -1,57 +1,46 @@
 grafo = {'a': [('p',4), ('j',15), ('b',1)],
-'b': [('a',1), ('d',2), ('e',2), ('c',3)],
-'j': [('a',15)],
-'p': [('a', 4)],
-'d': [('b',2), ('g',3)],
-'e': [('b',2), ('g',4), ('f',5), ('c',2)],
-'c': [('b',3), ('e',2), ('f',5), ('i',20)],
-'g': [('d',3), ('e',4), ('f',10), ('h',1)],
-'f': [('g',10), ('e',5), ('c',5)],
-'i': [('c',20)],
-'h': [('g',1)]
+         'b': [('a',1), ('d',2), ('e',2), ('c',3)],
+         'j': [('a',15)],
+         'p': [('a', 4)],
+         'd': [('b',2), ('g',3)],
+         'e': [('b',2), ('g',4), ('f',5), ('c',2)],
+         'c': [('b',3), ('e',2), ('f',5), ('i',20)],
+         'g': [('d',3), ('e',4), ('f',10), ('h',1)],
+         'f': [('g',10), ('e',5), ('c',5)],
+         'i': [('c',20)],
+         'h': [('g',1)]
 }
-#MUESTRA EL GRAFO ANTES DEL RECORRIDO
+
+# MUESTRA EL GRAFO ANTES DEL RECORRIDO
 print("Muestra el grafo antes del recorrido: \n")
 for key, lista in grafo.items():
     print(key)
     print(lista)
- 
 print()
 
 visitados = []
-pila = []
- 
+cola = []
 
 origen = input("Ingresa un nodo: ")
 print("\nLista de recorrido en anchura\n")
-#Paso 1: SE COLOCA EL VÉRTICE ORIGEN EN UNA PILA
-pila.append(origen)
-#Paso 2: MIENTRAS LA PILA NO ESTE VACÍA
-while pila:
-# paso 3: DESAPILAR UN VÉRTICE, ESTE SERÁ AHORA EL VÉRTICE ACTUAL
-  actual = pila.pop()
-   # FORMA ALTERNATIVA PARA DESAPILAR:
-   # actual = pila[1]
-   # pila.remove(pila[1])
 
+# Paso 1: SE COLOCA EL VÉRTICE ORIGEN EN UNA COLA
+cola.append(origen)
 
-   # paso 4: SI EL VÉRTICE ACTUAL NO HA SIDO VISITADO
+# Paso 2: MIENTRAS LA COLA NO ESTE VACÍA
+while cola:
+    # Paso 3: SACAMOS EL PRIMER VÉRTICE, ESTE SERÁ AHORA EL VÉRTICE ACTUAL
+    actual = cola.pop(0)
 
-if actual not in visitados:
-       # paso 5: PROCESAR (IMPRIMIR) EL VÉRTICE ACTUAL
-  print("Vertice actual > ", actual)
-   # paso 6: COLOCAR VÉRTICE ACTUAL EN LA LISTA DE VISITADOS
-  visitados.append(actual)
-
-   # paso 7: PARA CADA VÉRTICE QUE EL VÉRTICE ACTUAL TIENE COMO
-    # DESTINO,
-    # Y QUE NO HA SIDO VISITADO:
-   # APILAR EL VERTICE
-
-for key, lista in grafo[actual]:
-
-    if key not in visitados:
-      pila.append(key)
-
-
-print()
+    # Paso 4: SI EL VÉRTICE ACTUAL NO HA SIDO VISITADO
+    if actual not in visitados:
+        # Paso 5: PROCESAR (IMPRIMIR) EL VÉRTICE ACTUAL
+        print("Vertice actual >", actual)
+        # Paso 6: COLOCAR VÉRTICE ACTUAL EN LA LISTA DE VISITADOS
+        visitados.append(actual)
+        # Paso 7: PARA CADA VÉRTICE QUE EL VÉRTICE ACTUAL TIENE COMO DESTINO
+        for vecino, _ in grafo[actual]:
+            # Y QUE NO HA SIDO VISITADO
+            if vecino not in visitados and vecino not in cola:
+                # AGREGAR EL VÉRTICE A LA COLA
+                cola.append(vecino)
