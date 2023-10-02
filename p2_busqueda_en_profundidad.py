6c09ca9c2b9884a0429af12204d96e7d9f4ded4e
hecmung <kenwey3018@gmail.com>
2023-10-01 21:22:56 -0600

    Busqueda en profundidad completo

--- p2_busqueda_en_profundidad.py
+++ p2_busqueda_en_profundidad.py
@@ -22,18 +22,26 @@ def imprimir_arbol(nodo, indent=0):
 def dfs(inicio, objetivo):
     visitados = set()
     stack = [inicio]
+    camino = []
 
     while stack:
         nodo_actual = stack.pop()
+        camino.append(nodo_actual)
+
+        print(f"Visitando nodo: {nodo_actual}, Stack actual: {stack}")  # Imprimimos el seguimiento
 
         if nodo_actual == objetivo:
-            return True
+            return True, camino
 
         if nodo_actual not in visitados:
             visitados.add(nodo_actual)
-            stack.extend(arbol[nodo_actual])
+            hijos = list(arbol[nodo_actual])
+            hijos.reverse()  # invertimos el orden para explorar 'B' antes que 'C'
+            for hijo in hijos:
+                if hijo not in visitados:
+                    stack.append(hijo)
 
-    return False
+    return False, camino
 
 
 def main():
@@ -41,10 +49,14 @@ def main():
     imprimir_arbol('A')
     nodo_objetivo = input("\nIngresa el nodo objetivo (por ejemplo, 'F'): ").upper()
 
-    if dfs('A', nodo_objetivo):
+    es_alcanzable, camino = dfs('A', nodo_objetivo)
+
+    if es_alcanzable:
         print(f"\nEl nodo {nodo_objetivo} es alcanzable desde el nodo 'A'.")
+        print(f"Camino: {' -> '.join(camino)}")
     else:
         print(f"\nEl nodo {nodo_objetivo} no es alcanzable desde el nodo 'A'.")
+        print(f"Camino intentado: {' -> '.join(camino)}")
 
 
 main()
